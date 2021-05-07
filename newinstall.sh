b7527c84b88e7c88a90f4d60bb1d557840ad2ef6
samuelhogg <45640681+samuelhogg@users.noreply.github.com>
2021-05-07 22:49:54 +0100

    Update newinstall.sh

--- newinstall.sh
+++ newinstall.sh
@@ -2,5 +2,6 @@
 
 # wget -O - https://raw.githubusercontent.com/samuelhogg/scripts/main/newinstall.sh | bash
 
+# Download and Install Visual Studio Code (https://code.visualstudio.com/download)
 curl -L "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64" -o ~/Downloads/vscode.deb
 sudo apt install ~/Downloads/vscode.deb
